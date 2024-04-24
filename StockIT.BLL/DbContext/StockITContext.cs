1ad394a1dcc1f1b46b29659de32a3a4915e74327
stere8 <twizoraph4247@gmail.com>
2024-04-24 13:10:38 +0200

    *About me added

--- StockIT.BLL/DbContext/StockITContext.cs
+++ StockIT.BLL/DbContext/StockITContext.cs
@@ -11,7 +11,7 @@ public class StockITContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // Configure connection string to your database here
-        optionsBuilder.UseSqlServer("Server=tcp:oracleconsults.database.windows.net,1433;Initial Catalog=StockIt;Persist Security Info=False;User ID=oreste.twizeyimana;Password=Raphael47@Microsoft;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=100;"
+        optionsBuilder.UseSqlServer("Server=tcp:oracleconsults.database.windows.net,1433;Initial Catalog=StockIt;Persist Security Info=False;User ID=oreste.twizeyimana;Password=Raphael47@Microsoft;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
             );
     }
     public StockITContext(DbContextOptions<StockITContext> options) : base(options)
