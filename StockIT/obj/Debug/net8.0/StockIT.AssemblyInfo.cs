50469fcaa07f5acde547829cac123deeeff0680e
stere8 <twizoraph4247@gmail.com>
2025-09-29 19:45:47 +0200

    Update assembly version and cache file
    Updated the `AssemblyInformationalVersionAttribute` in `StockIT.AssemblyInfo.cs` to reflect a new build or commit hash. The version string was changed from `1.0.0+dcf64b60f206f674d0b1252783eabc19bbba4b28` to `1.0.0+30775fcac86429dbc9a521237bf4bf3fce0fa118`.
    
    Additionally, updated the `StockIT.AssemblyInfoInputs.cache` file to reflect the new state of the assembly information. The hash value was updated from `9c792c13dce02f3b0dbea9a9a0222e325b6bd4ad2eec661d4f0ed296568f7452` to `c4e964ec915f57d1f8b620ad2e409a9c987ab59b0e5bd429acd4d93bea32b23b`.
    Update publish profile and add CI/CD workflow
    
    Updated the `<NameOfLastUsedPublishProfile>` in `StockIT.csproj.user` to reference `stockit1.yml`. Updated the `AssemblyInformationalVersionAttribute` in `StockIT.AssemblyInfo.cs` and the hash in `StockIT.AssemblyInfoInputs.cache`.
    
    Added a new GitHub Actions workflow (`stockit1.yml`) to automate the build, test, and deployment of the .NET Core application to an Azure Web App. The workflow includes steps for restoring dependencies, building, testing, publishing, and deploying the application.

--- StockIT/obj/Debug/net8.0/StockIT.AssemblyInfo.cs
+++ StockIT/obj/Debug/net8.0/StockIT.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0f7d2ffca26b6415c06c566a2a4c5f45a62f0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30775fcac86429dbc9a521237bf4bf3fce0fa118")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
